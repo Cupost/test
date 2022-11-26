89b2936d6862adf6bb8ac7faa71daa6c76492b50
Cupost <le627959@gmail.com>
2022-11-26 20:54:15 +0700

    first commit

---
+++ sgk96.py
@@ -0,0 +1,7 @@
+print('nhap so giay')
+a = int(input())
+ngay = a//86400
+gio = a//3600 - ngay*24
+phut = a//60 - 60*24*ngay - gio*60
+giay = a - ngay*24*60*60 - gio*60*60 - phut*60
+print(ngay,'ngày',gio,'giờ',phut,'phút',giay,'giây')
