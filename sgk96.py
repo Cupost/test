05f2e04cb3e8a352217b01d1ca87793dc8cbe79c
Cupost <le627959@gmail.com>
2022-11-27 09:43:21 +0700

    reset

--- sgk96.py
+++ sgk96.py
@@ -6,5 +6,4 @@ phut = a//60 - 60*24*ngay - gio*60
 giay = a - ngay*24*60*60 - gio*60*60 - phut*60
 print(ngay,'ngày',gio,'giờ',phut,'phút',giay,'giây')
 #bai tap sgk/96 code python 
-print('done')
-print('complete')
+
