6f7e306a6d651bba168a4cc19f6bb1d6b893c749
Cupost <le627959@gmail.com>
2022-12-04 20:58:23 +0700

    sgk 100 van dung 2

--- sgk100-VD1.py
+++ sgk100-VD1.py
@@ -1,8 +1,13 @@
-print('nhap so giay')
+print('nhap so giay') 
 a = int(input())
+# Nhập số giây từ bàn phím 
+
 ngay = a//86400
 gio = a//3600 - ngay*24
 phut = a//60 - 60*24*ngay - gio*60
 giay = a - ngay*24*60*60 - gio*60*60 - phut*60
+# Quy đổi 
+
 print(ngay,'ngày',gio,'giờ',phut,'phút',giay,'giây')
-#bai tap sgk/96 code python 
+# In ra màn hình 
+
--- sgk100-VD2.py
+++ sgk100-VD2.py
@@ -1,10 +1,17 @@
 import math 
+# Khai báo thư viện 
+
 a = int(input())
 b = int(input())
 c = int(input())
+# Nhập số từ bàn phím 
+
 d = a+b+c
 p = d/2
 e = p*(p-a)*(p-b)*(p-c)
+# Quy đổi bằng công thức toán học
+
 print(math.sqrt(e))
 print(d)
+# In ra màn hình 
 
