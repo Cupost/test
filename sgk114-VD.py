cbdc9dba07d011ad322aa25f53a5fbd4fa3d436e
Cupost <le627959@gmail.com>
2022-12-06 10:15:43 +0700

    sgk 114 van dung

--- sgk114-VD.py
+++ sgk114-VD.py
@@ -1,6 +1,6 @@
-a = [1,2,3,4,9,5,6]
-max = max(a)
-for i in range(len(a)):
-    if a[i]==max:
-        print(i)
+a = [20,1,2,3,4,9,5,6,11]
+max = a[1]
+for i in a:
+    if i>max:
+        max = i 
 print(max)
